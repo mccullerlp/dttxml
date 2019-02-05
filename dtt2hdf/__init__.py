e8d830c09679c16b554acd21ad9a5eb0f124eded
Lee McCuller <Lee.McCuller@gmail.com>
2019-02-05 11:29:47 -0500

    fixed for versioning

--- dtt2hdf/__init__.py
+++ dtt2hdf/__init__.py
@@ -1,3 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+"""
 from .parse import read_diaggui
 from .access import DiagAccess
 from .dtt2hdf import diag_to_bunch
+
+from .version import (
+    version,
+    __version__,
+)
+
